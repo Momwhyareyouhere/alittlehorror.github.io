840a4d195c6604ea8a0b791827c8e339b065f3f2
Momwhyareyouhere <151050626+Momwhyareyouhere@users.noreply.github.com>
2024-02-05 18:51:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,6 @@ function changeEyeColor() {
 }
 
 setInterval(changeEyeColor, 2000);
+
+const monster = document.getElementById('monster');
+monster.style.backgroundImage = "url('horrifying_monster.png')";
