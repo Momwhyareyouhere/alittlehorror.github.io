dc3a810518739207b033b8e3784f45804ef0075e
Momwhyareyouhere <151050626+Momwhyareyouhere@users.noreply.github.com>
2024-02-05 18:52:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,30 @@ function getRandomColor() {
     return color;
 }
 
-function changeEyeColor() {
-    const eye = document.getElementById('eye');
-    const randomColor = getRandomColor();
-    eye.style.backgroundColor = randomColor;
+function createMonster() {
+    const monster = document.getElementById('monster');
+    monster.innerHTML = ''; 
+
+
+    const eye = document.createElement('div');
+    eye.className = 'monster-eye';
+    eye.style.backgroundColor = getRandomColor();
+
+    const blood = document.createElement('div');
+    blood.className = 'monster-blood';
+
+    const knife = document.createElement('div');
+    knife.className = 'monster-knife';
+
+
+    monster.appendChild(eye);
+    monster.appendChild(blood);
+    monster.appendChild(knife);
+
+
+    setInterval(() => {
+        eye.style.backgroundColor = getRandomColor();
+    }, 2000);
 }
 
-setInterval(changeEyeColor, 2000);
-
-const monster = document.getElementById('monster');
-monster.style.backgroundImage = "url('horrifying_monster.png')";
+createMonster();
